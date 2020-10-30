6571338989c93763c7213a6bc810af72f8f7e8c3
ad <dfsd@nave.com>
2020-10-30 17:48:44 +0900

    연산자,인터페이스,참조변수형변환,추상클래스,import

---
+++ JAVA/src/Day10/import에제.java
@@ -0,0 +1,13 @@
+package Day10;
+
+import static java.lang.Math.random;
+import static java.lang.System.out;
+
+public class import에제 {
+
+	public static void main(String[] args) {
+		System.out.println(random());
+		out.println(random());
+	}
+
+}
